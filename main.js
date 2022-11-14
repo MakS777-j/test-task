8aada607239c6e5f3a6b13d5e3ecec82b563d562
MakS777-j <skmaks777@gmail.com>
2022-11-14 19:49:44 +0200

    eight

--- main.js
+++ main.js
@@ -1,57 +1,34 @@
 
-function show(){
+function selectLi(id){
+    // document.querySelectorAll("li").forEach(function(listItem){
+    //     listItem.addEventListener('click', function(){
+            
+    //         document.getElementById(id).value = this.innerText;
+    //         let elem = document.querySelector("ul");
+    //         elem.classList.remove("click");
+            
+    //     })
+    // })
+    document.getElementById(id).value = this.innerText;
+    let elem = document.querySelector("ul");
+    elem.classList.remove("click");
 
-    // let elem = document.querySelector("ul");
-    // elem.classList.toggle("click");
-
-    // let elem = document.getElementsByClassName("select");
-    // elem[1].classList.toggle("click");
 }
 
-
-document.getElementById("Practice").addEventListener('click', function(){
-    let elem = document.getElementsByClassName("select");
-    elem[0].classList.toggle("click");
-})
-
-document.getElementById("Medical").addEventListener('click', function(){
-    let elem = document.getElementsByClassName("select");
-    elem[1].classList.toggle("click");
-})
-
-// document.querySelectorAll(".choice").forEach(function(list){
-//     list.addEventListener('click', function(){
-//         let elem = document.getElementsByClassName("select");
+document.querySelectorAll(".choice").forEach(function(list){
+    list.addEventListener('click', function(){
+        let elem = document.getElementsByClassName("select");
         
-//         for(i in elem){
-//             if (this.id = "Practice"){
-//                 elem[0].classList.toggle("click");
-//                 break
-//             }
-//             if (this.id = "Medical"){
-//                 elem[1].classList.toggle("click");
-//                 break
-//             }
-//         }   
-        
-//     })
-// })
-
-
-document.querySelectorAll("li").forEach(function(listItem){
-    listItem.addEventListener('click', function(){
-        
-        document.getElementById("Practice").value = this.innerText;
-        let elem = document.querySelector("ul");
-        elem.classList.remove("click");
+        id = this.id;
+        if(id == "Medical"){
+            elem[1].classList.toggle("click");
+            selectLi(id);
+        }
+        else{
+            elem[0].classList.toggle("click");
+            selectLi(id)
+        }
     })
 })
 
-document.querySelectorAll("li").forEach(function(listItem){
-    listItem.addEventListener('click', function(){
-        
-        document.getElementById("Medical").value = this.innerText;
-        let elem = document.querySelector("ul");
-        elem.classList.remove("click");
-    })
-})
+
